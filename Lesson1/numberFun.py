d80325cde516651c616e1f09b9526149e5e99206
Samantha Sortzi <sortzis@mail.nmc.edu>
2021-01-19 22:05:44 -0500

    Initial Commit

---
+++ Lesson1/numberFun.py
@@ -0,0 +1,24 @@
+import random
+number1 = random.randrange(1,100)
+number2 = random.randrange(1,100)
+
+
+print("First number= ",number1)
+print("Second number= ",number2)
+print(number1, "/", number2, "=",number1/number2)
+print(number1, "//", number2, "=",number1//number2)
+print(number1, "%", number2, "=",number1%number2)
+print(number1, "+", number2, "=",number1+number2)
+print(number1, "-", number2, "=",number1-number2)
+print(number1, "*", number2, "=",number1*number2)
+
+NUMBER2 = 25
+number2 = NUMBER2
+print("First number= ",number1)
+print("Second number= ",number2)
+print(number1, "/", number2, "=",number1/number2)
+print(number1, "//", number2, "=",number1//number2)
+print(number1, "%", number2, "=",number1%number2)
+print(number1, "+", number2, "=",number1+number2)
+print(number1, "-", number2, "=",number1-number2)
+print(number1, "*", number2, "=",number1*number2)
---
+++ Lesson1/stringFun.py
@@ -0,0 +1,45 @@
+# exercise 1
+print("--------------------------------------")
+print("Exercise 1")
+name="sammi sortzi"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("My first initial is: ", name[0].upper())
+
+#exercise 2
+print("--------------------------------------")
+print("Exercise 2")
+noun = "fish"
+adj = "quickly"
+verb = "swam"
+ending = "to the end of the ocean"
+sentence1 = "the " + noun + " " + adj + " " + verb + " " + ending
+sentence2 =f"the {noun} {adj} {verb} {ending}"
+print(sentence1.upper())
+print(sentence2.lower())
+
+#exercise 3
+print("---------------------------------------")
+print("Exercise 3")
+poem = """We gave you the chance
+to water the plants.
+We didn't mean that way-
+Now zip up your pants."""
+print(poem.lower())
+
+#exercise 4
+print("--------------------------------------")
+print("Exercise 4")
+string1 = "drink"
+string2 = "coke"
+string3 = "pepsi"
+string4 = "rootbeer"
+value1 = "snack"
+value2 = "chips"
+value3 = "pretzels"
+value4 = "popcorn"
+print(string1,"\t\t",value1,"\n")
+print(string2,"\t\t",value2,"\n")
+print(string3,"\t\t",value3,"\n")
+print(string4,"\t",value4,"\n")
