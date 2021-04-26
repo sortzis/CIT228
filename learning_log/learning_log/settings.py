7541cbbe19e53023dd2b61352b8d02eef872d8aa
Samantha Sortzi <sortzis@mail.nmc.edu>
2021-04-26 18:57:15 -0400

    Chapter19

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps
     'learning_logs',
+    'users',
 
     #Default django apps.
     'django.contrib.admin',
@@ -127,3 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings
+LOGIN_URL = 'users:login'
