5c65f5edfdd1c039a7375017cdcb7f8e19986ce3
sortzis <eric@example.com>
2021-04-27 03:23:27 -0400

    Chapter20

---
+++ California_Info/earthquakes_week.py
@@ -0,0 +1,45 @@
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+#Explore the structure of the data.
+filename = 'California_Info/all_week.json'
+with open(filename, encoding="utf8") as f:
+    all_eq_data = json.load(f)
+
+#readable_file = 'California_Info/readable_eq_data.json'
+#with open(readable_file, 'w') as f:
+    #json.dump(eq_data, f, indent=4)
+
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats, hover_texts = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    hover_texts.append(title)
+
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_texts,
+    'marker': {
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'YlGnBu',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
+    },
+}]
+
+my_layout = Layout(title='California Earthquakes in the last Week')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='Chapter16/earthquakesweek.html')
